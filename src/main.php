80e086f851c50719a4b30e340836f6ae2f1b4f37
Dr. Nab Roshyara <mathematikboy@yahoo.com>
2021-02-23 23:51:29 +0100

    readme edited

--- src/main.php
+++ src/main.php
@@ -4,7 +4,20 @@
     
 <div class="container" >
 <div class="flex flex-col text-2xl py-4">      
-    <p class="mx-auto"> The bus stops are marked with numbers. Buses routes are either only one way or two ways. You can see the name of the Bus stops  when youput your mouse on the stops.  </p>
+    <p class="mx-auto"> The Underlying map is google map and  Bus stops are marked with numbers in the google map. Buses routes are either only one way or two ways. A detailed informaton on the one or two way route is described the file:
+     <b> final_graph.php </b>. Each Bus stop is marked as the coordinate system.  
+      <p class=" my-4 px-4 "> 
+        e.g.<br> 
+     <span class="font-semibold text-gray-700">  From stop 1, you can go to Stop 3  from stop 2 to 3, and so on.</span> <br> 
+      '1'  => array( '3' => 0.0169508729 ), <br>
+      '2'  => array( '3' => 0.0340175589 ), <br>
+      '3'  => array( '5' => 0.0179478624 ), <br>
+      '4'  => array( '3' => 0.0530424069 ), <br>
+      '5'  => array( '8' => 0.0109678852 ),<br>
+      '47'  => array( '412' => 0.0428871797,  '406' => 0.0049853733 )<br>
+       From 47 to 412 and then from 412 to 406  <br>
+     </p>
+    You can see the name of the Bus stops  when youput your mouse on the stops.  </p>
     <p class="px-2 font-semibold text-gray-800 py-4 mx-auto">  You are interested to travel from one palce to other? No problem at all. <br> 
 
      Just select th start and end stops of your route. If there is an  exsiting route  between the start and end stops, this program will display the shortest route for you.  To display the route, please click on the button:
